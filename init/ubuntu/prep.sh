44a539d11f27b7f321bc5b8177f3f92e7df532d8
chlorochrule <minami.polly@gmail.com>
2018-05-01 22:59:08 +0900

    Delete specific directory `ubuntu/mac`

--- init/ubuntu/prep.sh
+++
@@ -1,9 +0,0 @@
-# apt
-sudo apt install xsel xdotool build-essential
-
-# linuxbrew
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
-test -d ~/.linuxbrew && PATH="$HOME/.linuxbrew/bin:$HOME/.linuxbrew/sbin:$PATH"
-test -d /home/linuxbrew/.linuxbrew && PATH="/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin:$PATH"
-test -r ~/.bash_profile && echo "export PATH='$(brew --prefix)/bin:$(brew --prefix)/sbin'":'"$PATH"' >>~/.bash_profile
-echo "export PATH='$(brew --prefix)/bin:$(brew --prefix)/sbin'":'"$PATH"' >>~/.profile
